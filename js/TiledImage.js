a58172a37f24d33645e2775d8e24a8ba6a5f85d7
Frederic Theriault <fred.theriault@gmail.com>
2017-01-10 14:30:17 -0500

    Now supporting loop or not.
    Fixed indentation in JS

--- js/TiledImage.js
+++ js/TiledImage.js
@@ -1,23 +1,24 @@
- function TiledImage(imagePath, columns, rows, refreshInterval, horizontal, scale) {
+function TiledImage(imagePath, columns, rows, refreshInterval, horizontal, scale) {
 	this.imageList = new Array();
 	this.tickTime = 0;
 	this.tickDrawFrameInterval = 0;
 	this.tickRefreshInterval = 100;
-    this.horizontal = true;
-    this.scale = 1.0;
-    this.flipped = false;
+	this.horizontal = true;
+	this.scale = 1.0;
+	this.flipped = false;
+	this.looped = true;
 
-    if (scale !== undefined) {
-    	this.scale = scale;
-    }
+	if (scale !== undefined) {
+	   this.scale = scale;
+	}
 
-    if (horizontal !== undefined) {
-        this.horizontal = horizontal;
-    }
+	if (horizontal !== undefined) {
+	   this.horizontal = horizontal;
+	}
 
-    if (refreshInterval !== undefined) {
-        this.tickRefreshInterval = refreshInterval;
-    }
+	if (refreshInterval !== undefined) {
+	   this.tickRefreshInterval = refreshInterval;
+	}
 
 	var image = new Image();
 	image.src = imagePath;
@@ -31,8 +32,12 @@
 }
 
 TiledImage.prototype.setFlipped = function (flipped) {
-    this.flipped = flipped;
+	this.flipped = flipped;
 };
+
+TiledImage.prototype.setLooped = function (looped) {
+	this.looped = looped;
+}
 
 TiledImage.prototype.addImage = function(imagePath) {
 	var image = new Image();
@@ -44,20 +49,20 @@ TiledImage.prototype.addImage = function(imagePath) {
 TiledImage.prototype.changeRow = function(row) {
 	this.imageCurrentRow = row;
 
-    if (!this.horizontal) {
-         this.imageAnimationMin = row;
-         this.imageAnimationMax = row;
-    }
+	if (!this.horizontal) {
+		this.imageAnimationMin = row;
+		this.imageAnimationMax = row;
+	}
 }
 
 // starts at 0
 TiledImage.prototype.changeCol = function(col) {
 	this.imageCurrentCol = col;
 
-    if (this.horizontal) {
-         this.imageAnimationMin = col;
-         this.imageAnimationMax = col;
-    }
+	if (this.horizontal) {
+		this.imageAnimationMin = col;
+		this.imageAnimationMax = col;
+	}
 }
 
 // starts at 0
@@ -65,18 +70,18 @@ TiledImage.prototype.changeMinMaxInterval = function (min, max) {
 	this.imageAnimationMin = min;
 	this.imageAnimationMax = max;
 
-    if (this.horizontal) {
-    	if (this.imageCurrentCol < this.imageAnimationMin ||
-    		this.imageCurrentCol >= this.imageAnimationMax) {
-    		this.imageCurrentCol = this.imageAnimationMin;
-    	}
-    }
-    else {
-        if (this.imageCurrentRow < this.imageAnimationMin ||
-    		this.imageCurrentRow >= this.imageAnimationMax) {
-    		this.imageCurrentRow = this.imageAnimationMin;
-    	}
-    }
+	if (this.horizontal) {
+		if (this.imageCurrentCol < this.imageAnimationMin ||
+		   this.imageCurrentCol > this.imageAnimationMax) {
+		   this.imageCurrentCol = this.imageAnimationMin;
+		}
+	}
+	else {
+		if (this.imageCurrentRow < this.imageAnimationMin ||
+		   this.imageCurrentRow > this.imageAnimationMax) {
+		   this.imageCurrentRow = this.imageAnimationMin;
+		}
+	}
 }
 
 TiledImage.prototype.resetCol = function () {
@@ -96,46 +101,46 @@ TiledImage.prototype.tick = function (ctx, spritePosX, spritePosY) {
 	for (var i = 0; i < this.imageList.length;i++) {
 
 		if (this.imageList[i].complete) {
-            if (this.flipped) {
-                ctx.save();
-                ctx.translate(Math.floor(spritePosX - this.imageList[i].width/this.imageTileColCount/2)  +
-                				this.imageList[i].width/this.imageTileColCount * this.scale,
-                			  Math.floor(spritePosY - this.imageList[i].height/this.imageTileRowCount/2));
-                ctx.scale(-1, 1);
-            }
+			if (this.flipped) {
+				ctx.save();
+				ctx.translate(Math.floor(spritePosX - this.imageList[i].width/this.imageTileColCount/2)  +
+							   this.imageList[i].width/this.imageTileColCount * this.scale,
+							 Math.floor(spritePosY - this.imageList[i].height/this.imageTileRowCount/2));
+				ctx.scale(-1, 1);
+			}
 
 			ctx.drawImage(this.imageList[i],
-						  this.imageList[i].width/this.imageTileColCount * this.imageCurrentCol,
-						  this.imageList[i].height/this.imageTileRowCount * this.imageCurrentRow,
-						  this.imageList[i].width/this.imageTileColCount,
-						  this.imageList[i].height/this.imageTileRowCount,
-						  this.flipped ? 0 : Math.floor(spritePosX - this.imageList[i].width/this.imageTileColCount/2),
-						  this.flipped ? 0 : Math.floor(spritePosY - this.imageList[i].height/this.imageTileRowCount/2),
-						  this.imageList[i].width/this.imageTileColCount * this.scale,
-						  this.imageList[i].height/this.imageTileRowCount * this.scale);
+						 this.imageList[i].width/this.imageTileColCount * this.imageCurrentCol,
+						 this.imageList[i].height/this.imageTileRowCount * this.imageCurrentRow,
+						 this.imageList[i].width/this.imageTileColCount,
+						 this.imageList[i].height/this.imageTileRowCount,
+						 this.flipped ? 0 : Math.floor(spritePosX - this.imageList[i].width/this.imageTileColCount/2),
+						 this.flipped ? 0 : Math.floor(spritePosY - this.imageList[i].height/this.imageTileRowCount/2),
+						 this.imageList[i].width/this.imageTileColCount * this.scale,
+						 this.imageList[i].height/this.imageTileRowCount * this.scale);
 
-            if (this.flipped) {
-                ctx.restore();
-            }
+			if (this.flipped) {
+				ctx.restore();
+			}
 
 			if (this.tickDrawFrameInterval == 0) {
-                if (this.horizontal) {
-    				this.imageCurrentCol++;
-
-    				if (this.imageCurrentCol < this.imageAnimationMin ||
-    					this.imageCurrentCol >= this.imageAnimationMax) {
-    					this.imageCurrentCol = this.imageAnimationMin;
-    				}
-                }
-                else {
-                    this.imageCurrentRow++;
-
-    				if (this.imageCurrentRow < this.imageAnimationMin ||
-    					this.imageCurrentRow >= this.imageAnimationMax) {
-    					this.imageCurrentRow = this.imageAnimationMin;
-    				}
-                }
+				if (this.horizontal) {
+					if (this.imageCurrentCol + 1 >= this.imageAnimationMax) {
+						if (this.looped) this.imageCurrentCol = this.imageAnimationMin;
+					}
+					else {
+						this.imageCurrentCol++;
+					}
+				}
+				else {
+					if (this.imageCurrentRow + 1 >= this.imageAnimationMax) {
+						if (this.looped) this.imageCurrentRow = this.imageAnimationMin;
+					}
+					else {
+						this.imageCurrentRow++;
+					}
+				}
 			}
-		}
+	   }
 	}
 }
